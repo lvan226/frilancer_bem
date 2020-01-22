8baf98e71f97bf38a50c18186a5fff988334addb
lvan226 <lvan226@list.ru>
2020-01-22 12:27:25 +0800

    Added menu mobile

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,15 @@
 $(function(){
+  let menuBurger = document.querySelector('.menu-burger');
+  let menuList = document.querySelector('.menu-list');
 
-
-
+  menuBurger.addEventListener('click', () => {
+    if(menuList.classList.contains('menu-list_active')) {
+      menuList.classList.remove('menu-list_active');
+      menuBurger.classList.remove('menu-burger_active');
+    }else {
+      menuList.classList.add('menu-list_active');
+      menuBurger.classList.add('menu-burger_active');
+    }
+  });
   
 });
